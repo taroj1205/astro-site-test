55920b50f94cc7bddf55fa7c2f4ddac5432c0fd3
taroj1205 <taroj1205@gmail.com>
2024-11-04 12:52:56 +1300

    fix(layout): adjust main container padding and correct add more variants to spacing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,10 @@ const plugin = require("tailwindcss/plugin");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    containr: {
+    container: {
       padding: {
         DEFAULT: "1rem",
+        xs: "0.5rem",
         sm: "2rem",
         lg: "4rem",
         xl: "5rem",
@@ -18,9 +19,19 @@ module.exports = {
         muted: "hsl(258, 48%, 55%)",
       },
       spacing: {
-        lg: "2.5rem",
-        md: "1.5rem",
+        xs: "0.5rem",
         sm: "1rem",
+        md: "1.5rem",
+        lg: "2.5rem",
+        xl: "3rem",
+        "2xl": "4rem",
+        "3xl": "5rem",
+        "4xl": "6rem",
+        "5xl": "7rem",
+        "6xl": "8rem",
+        "7xl": "9rem",
+        "8xl": "10rem",
+        "9xl": "11rem",
       },
     },
   },
