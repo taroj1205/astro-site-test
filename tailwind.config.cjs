0baef9ac7f3e73bb5cc70f220137a10b37b3ad08
TheRealMG <56460873+TheRealMG@users.noreply.github.com>
2024-11-04 13:57:14 -0600

    rudimentary dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,7 +17,9 @@ module.exports = {
     extend: {
       colors: {
         primary: "hsl(258, 48%, 23%)",
+        "primary-dark": "hsl(258, 48%, 65%)",
         muted: "hsl(258, 48%, 55%)",
+        "muted-dark": "hsl(258, 48%, 85%)",
       },
       spacing: {
         xs: "0.5rem",
@@ -50,13 +52,20 @@ module.exports = {
         ],
         dm: ["DM Sans"],
       },
+      dropShadow: {
+        glow: [
+          "0 0px 5px hsla(258, 48%, 23%, 0.90)",
+          "0 0px 20px hsla(258, 48%, 23%, 0.75)"
+        ]
+      }
     },
   },
   plugins: [
     plugin(({ addBase, addComponents, addUtilities, theme }) => {
       addBase({
         h2: {
-          "@apply text-scale-large font-bold text-primary": {},
+          "@apply text-scale-large font-bold text-primary dark:text-primary-dark": {},
+
         },
       });
       addComponents({
@@ -71,7 +80,8 @@ module.exports = {
           "@apply text-center place-items-center": {},
         },
         ".btn-base": {
-          "@apply bg-black text-white font-semibold py-3 px-6 rounded-full w-fit flex items-center gap-2 text-lg drop-shadow-lg hover:drop-shadow-xl transition-all duration-300":
+          "@apply bg-black text-white font-semibold py-3 px-6 rounded-full w-fit flex items-center gap-2 text-lg drop-shadow-lg hover:drop-shadow-xl transition-all duration-300 dark:bg-white dark:text-black":
+
             {},
         },
       });
