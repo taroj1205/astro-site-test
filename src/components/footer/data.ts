6e1f2cbdd4ec8dd6d2cf1dfc37669e399d5579ba
taroj1205 <taroj1205@gmail.com>
2024-11-12 00:25:37 +1300

    feat(footer): replace footer component with new structure and links

---
+++ src/components/footer/data.ts
@@ -0,0 +1,83 @@
+export const links = [
+  {
+    title: "Get Started",
+    links: [
+      {
+        title: "Mods",
+        href: "/mods",
+      },
+      {
+        title: "Download",
+        href: "/download",
+      },
+      {
+        title: "Create a Theme",
+        href: "/create-theme",
+      },
+      {
+        title: "Download Twilight",
+        href: "/twilight",
+      },
+    ],
+  },
+  {
+    title: "Get Help",
+    links: [
+      {
+        title: "Discord",
+        href: "/discord",
+      },
+      {
+        title: "Report an Issue",
+        href: "/issues",
+      },
+    ],
+  },
+  {
+    title: "About Us",
+    links: [
+      {
+        title: "About the team",
+        href: "/about",
+      },
+      {
+        title: "Privacy Policy",
+        href: "/privacy",
+      },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      {
+        title: "Branding Assets",
+        href: "/brand",
+      },
+      {
+        title: "Source Code",
+        href: "/source",
+      },
+      {
+        title: "Documentation",
+        href: "/docs",
+      },
+      {
+        title: "Release Notes",
+        href: "/releases",
+      },
+    ],
+  },
+  {
+    title: "Support Us",
+    links: [
+      {
+        title: "Patreon",
+        href: "https://patreon.com/zenbrowser",
+      },
+      {
+        title: "Ko-fi",
+        href: "https://ko-fi.com/zenbrowser",
+      },
+    ],
+  },
+];
